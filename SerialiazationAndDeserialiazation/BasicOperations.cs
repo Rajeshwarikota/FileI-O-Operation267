0c7e5ca4f18ad6e36916c1e047584a626dd93d51
Rajeshwarikota <kotarajeswari003.com>
2023-04-08 19:33:52 +0530

    [Rajeshwari kota] Adding Basic Operations

---
+++ SerialiazationAndDeserialiazation/BasicOperations.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialiazationAndDeserialiazation
+{
+    internal class BasicOperations
+    {
+        public static void Operations()
+        {
+
+            bool chech = true;
+
+            while (chech)
+            {
+                string path1 = ("C:/Users/LENOVO/source/repos/FileOperations/FileOperations/abcd.txt");
+                string path2 = ("C:/Users/LENOVO/source/repos/FileOperations/FileOperations/TextFile.txt");
+                Console.WriteLine("1) To read all Lines\n" +
+                                   "2) To Read All Text\n" +
+                                   "3) To Write All Text\n" +
+                                   "4) To Append\n" +
+                                   "5) To Delete Path\n" +
+                                   "6) using FileStream\n" +
+                                   "7) using FileStream and StreamWriter\n" +
+                                   "8) using FileStream and StreamReader\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        string[] Line;
+                        Line = File.ReadAllLines(path1);
+                        Console.WriteLine(Line[0]);
+                        Console.WriteLine(Line[1]);
+                        break;
+                    case 2:
+                        string Line2;
+                        Line2 = File.ReadAllText(path1);
+                        Console.WriteLine(Line2);
+                        break;
+                    case 3:
+                        string[] data = { "Raji", "swetha", "Apurva" };
+                        File.WriteAllLines(path1, data);
+                        break;
+                    case 4:
+                        string data1 = "vinny";
+                        File.AppendAllText(path1, data1);
+                        Console.WriteLine(data1);
+                        break;
+                    case 5:
+                        File.Delete(path1);
+                        break;
+                    case 6:
+                        FileStream file = new FileStream(path2, FileMode.Create);
+                        {
+                            Console.WriteLine("File is created");
+                            file.Close();
+                        }
+                        break;
+                    case 7:
+                        using (FileStream file1 = new FileStream(path2, FileMode.Create))
+                        {
+                            Console.WriteLine("File is created");
+                            using (StreamWriter writer = new StreamWriter(file1))
+                            {
+                                writer.WriteLine("Rajeshwari kota");
+                            }
+                        }
+                        break;
+                    case 8:
+                        using (FileStream file2 = new FileStream(path2, FileMode.Open))
+                        using (StreamReader reader = new StreamReader(file2))
+                        {
+                            Console.WriteLine("File is open");
+                            string fileContent = reader.ReadToEnd();
+                            Console.WriteLine(fileContent);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
--- SerialiazationAndDeserialiazation/Program.cs
+++ SerialiazationAndDeserialiazation/Program.cs
@@ -11,6 +11,15 @@ namespace SerialiazationAndDeserialiazation
         static void Main(string[] args)
         {
             Console.WriteLine("....File I/O Operations....");
+            Console.WriteLine("...Choose the Below Option...");
+            Console.WriteLine("\n1:Basic Operations\n2:Binary operations\n3:Xml operations\n4:Csv operations\n5:JSON operations\n22:Default");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    BasicOperations.Operations();
+                    break;
+            }
             Console.ReadLine();
         }
     }
